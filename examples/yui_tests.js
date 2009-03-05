6e1fdad2b0664af2d998e45ecfb43d2a65282ce0
Kernowsoul <jamie@kernowsoul.com>
2009-03-05 17:08:36 +0000

    finished first cut

--- examples/yui_tests.js
+++ examples/yui_tests.js
@@ -32,12 +32,99 @@ YAHOO.qMock.unitTests.singleMethod = new YAHOO.tool.TestCase({
 
 */
 
-myObj = {
-	alert_hooray: function(){alert('hooray!')}
-	//wierd: function(){},
+// START debugging functions 
+function inspect(obj, maxLevels, level)
+{
+ var str = '', type, msg;
+
+   // Start Input Validations
+   // Don't touch, we start iterating at level zero
+   if(level == null)  level = 0;
+
+   // At least you want to show the first level
+   if(maxLevels == null) maxLevels = 1;
+   if(maxLevels < 1)    
+       return '<font color="red">Error: Levels number must be > 0</font>';
+
+   // We start with a non null object
+   if(obj == null)
+   return '<font color="red">Error: Object <b>NULL</b></font>';
+   // End Input Validations
+
+   // Each Iteration must be indented
+   str += '<ul>';
+
+   // Start iterations for all objects in obj
+   for(property in obj)
+   {
+     try
+     {
+         // Show "property" and "type property"
+         type =  typeof(obj[property]);
+         str += '<li>(' + type + ') ' + property +
+                ( (obj[property]==null)?(': <b>null</b>'):('')) + '</li>';
+
+         // We keep iterating if this property is an Object, non null
+         // and we are inside the required number of levels
+         if((type == 'object') && (obj[property] != null) && (level+1 < maxLevels))
+         str += inspect(obj[property], maxLevels, level+1);
+     }
+     catch(err)
+     {
+       // Is there some properties in obj we can't access? Print it red.
+       if(typeof(err) == 'string') msg = err;
+       else if(err.message)        msg = err.message;
+       else if(err.description)    msg = err.description;
+       else                        msg = 'Unknown';
+
+       str += '<li><font color="red">(Error) ' + property + ': ' + msg +'</font></li>';
+     }
+   }
+
+     // Close indent
+     str += '</ul>';
+
+   return str;
+}
+
+// END debugging functions 
+
+Object.prototype.clone = function() {
+  var newObj = (this instanceof Array) ? [] : {};
+  for (i in this) {
+    if (i == 'clone') continue;
+    if (this[i] && typeof this[i] == "object") {
+      newObj[i] = this[i].clone();
+    } else newObj[i] = this[i]
+  } return newObj;
 };
 
-	
+function obj_the_same (firstObj, secondObj) {
+	for (var objInd in firstObj) {
+	    if (typeof(firstObj[objInd]) == 'object') {
+			if(secondObj[objInd]){
+	        	if(obj_the_same(firstObj[objInd], secondObj[objInd]) == false){
+					return false;
+				}
+			} else {
+				return false;
+			}
+	    } else {
+			if(secondObj[objInd] == false){
+				return false;
+			}
+	    }
+	}
+	return true;
+}
+
+
+myObj = {
+	alert_hooray: function(){alert('hooray!')},
+	alert_boo: function(){alert('boo!')}
+};
+
+
 YAHOO.namespace("qMock.unitTests");
    
    	YAHOO.qMock.unitTests.singleMethod = new YAHOO.tool.TestCase({
@@ -50,9 +137,12 @@ YAHOO.namespace("qMock.unitTests");
 		testMockExisting: function() {
 			mock = new Mock(myObj);
 			myObj.expects('alert_hooray').once().with(1,2).returns('a string');
-			myObj.alert_hooray(1,2,3);
+			myObj.expects('alert_boo').once().with('skdfsd').returns('a string');
+			myObj.alert_hooray(1,2);
+			//myObj.alert_hooray(1,2);
+			myObj.alert_boo('skdfsd');
 			//myObj.alert_hooray(1,2,3);
-		    assert(myObj.verify(), "object verification passed");
+		    assert(mock.verify(), "object verification passed");
 		}
 	});
  
--- jsmocha.js
+++ jsmocha.js
@@ -1,52 +1,8 @@
-/*
-function Mock(objectToMock) {
-	
-	if(objectToMock != null) {
-		
-		if(this.checkForClashes(objectToMock)){
-			throw new Error("Cannot mock object, function names clash!!");
-		}
-		
-		if( typeof(objectToMock) == 'function' ) {
-			// not dealing with functions for now
-		} 
-		else if( typeof(objectToMock) == 'object') {
-			this.addMethodsToObject(objectToMock);
-		} 
-		else {
-			throw new Error("Cannot mock something of type: " + typeof(objectToMock));
-		}
+// TODO move this to a better place
+object_to_array = function(o) {
+	var a = new Array();
+	for(var i = 0; i < o.length; i++){
+		a.push(o[i]);
 	}
-	else {
-		objectToMock = {};
-		this.addMethodsToObject(objectToMock);
-	}
-	return objectToMock;
-} 
-
-Mock.prototype = {
-	
-	reservedNames: ['expects', 'verify'],
-	
-	checkForClashes: function(object) {
-		for( property in object ){
-			if(this.reservedNames.inArray(property)){
-				return true;
-			}
-		}
-	},
-	
-	addMethodsToObject: function(object) {
-		object.expects = function(method_name){
-			alert('called expects for: '+ method_name);
-			// store the original methods
-			expectation = new Expectation(this, method_name);
-			//this.methods[method_name] = this[method_name];
-			//jsMocha.mockMethod(this, method_name);
-			return expectation;
-		};
-		
-		object.methods = [];
-	}
+	return a;
 }
-*/
