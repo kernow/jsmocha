f288dcf823c4d56e5b8d6b9aa658d703ef349eef
Jamie Dyer <jamie@kernowsoul.com>
2009-04-15 15:25:25 +0100

    moved lone inside class

--- jsmocha.js
+++
@@ -1,9 +0,0 @@
-// TODO move this to a better place
-// TODO better reporting of complex objects for parameters matcher
-object_to_array = function(o) {
-	var a = new Array();
-	for(var i = 0; i < o.length; i++){
-		a.push(o[i]);
-	}
-	return a;
-}
